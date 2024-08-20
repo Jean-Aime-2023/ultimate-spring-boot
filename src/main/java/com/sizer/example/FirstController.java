a3aeb50efb7152cb7e8ed88b95fba17666c8dcf3
Jean-Aime-2023 <124204227+Jean-Aime-2023@users.noreply.github.com>
2024-08-20 20:51:38 +0200

    updates : post and getting

--- src/main/java/com/sizer/example/FirstController.java
+++ src/main/java/com/sizer/example/FirstController.java
@@ -1,17 +1,35 @@
 package com.sizer.example;
 
+import java.util.*;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FirstController {
-    
-    @GetMapping
-    public String sayHello(){
-        return "Hello from my first controller";
+
+    private final StudentRepository repository;
+
+    public FirstController(StudentRepository repository) {
+        this.repository = repository;
     }
-    @GetMapping("/hello-2")
-    public String sayHello2(){
-        return "Hello from my 2 controller";
+
+    @PostMapping("/students")
+    public Student post(
+            @RequestBody Student student) {
+        return repository.save(student);
+    }
+
+    @GetMapping("/students")
+    public List<Student> findAllStudents() {
+        return repository.findAll();
+    }
+
+    @GetMapping("/students/{student-id}")
+    public Student findStudentById(
+            @PathVariable("student-id") Integer id) {
+        return repository.findById(id).orElse(new Student());
     }
 }
--- src/main/java/com/sizer/example/Student.java
+++ src/main/java/com/sizer/example/Student.java
@@ -1,6 +1,8 @@
 package com.sizer.example;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
@@ -9,39 +11,74 @@ import jakarta.persistence.Table;
 public class Student {
 
     @Id
+    @GeneratedValue
     private Integer id;
+    @Column(name = "c_fname", length = 20)
     private String firstname;
     private String lastname;
+    @Column(unique = true)
+    private String email;
     private int age;
-    public Student(){}
-    public Student(Integer id, String firstname, String lastname, int age) {
+    @Column(updatable = false)
+    private String some_column;
+
+    public Student() {
+    }
+
+    public Student(Integer id, String firstname, String lastname, String email, int age, String some_column) {
         this.id = id;
         this.firstname = firstname;
         this.lastname = lastname;
+        this.email = email;
         this.age = age;
+        this.some_column = some_column;
     }
+
     public Integer getId() {
         return id;
     }
+
     public void setId(Integer id) {
         this.id = id;
     }
+
     public String getFirstname() {
         return firstname;
     }
+
     public void setFirstname(String firstname) {
         this.firstname = firstname;
     }
+
     public String getLastname() {
         return lastname;
     }
+
     public void setLastname(String lastname) {
         this.lastname = lastname;
     }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
     public int getAge() {
         return age;
     }
+
     public void setAge(int age) {
         this.age = age;
+    }
+
+    public String getSome_column() {
+        return some_column;
+    }
+
+    public void setSome_column(String some_column) {
+        this.some_column = some_column;
     }
 }
