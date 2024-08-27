5ca4a926414c301dbda8f51131d0b1ba0ab64956
davinyiringabo <nyiringabodavid62@gmail.com>
2024-08-27 14:17:59 +0200

    updates : data validation

--- src/main/java/com/sizer/example/student/StudentController.java
+++ src/main/java/com/sizer/example/student/StudentController.java
@@ -1,15 +1,20 @@
 package com.sizer.example.student;
 
 import java.util.*;
-
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.validation.Valid;
 
 @RestController
 public class StudentController {
@@ -22,7 +27,7 @@ public class StudentController {
 
     @PostMapping("/students")
     public StudentResponseDto saveStudent(
-            @RequestBody StudentDto dto) {
+            @Valid @RequestBody StudentDto dto) {
        return this.studentService.saveStudent(dto);
     }
 
@@ -48,5 +53,18 @@ public class StudentController {
     @ResponseStatus(HttpStatus.OK)
     public void deleteStudent(@PathVariable("student-id") Integer id) {
         studentService.deleteStudent(id);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<?> handleMethodArgumentNotValidException(MethodArgumentNotValidException exp){
+        var errors = new HashMap<String,String>();
+        exp.getBindingResult().getAllErrors().forEach(error -> {
+            var fieldName = ((FieldError) error).getField();
+            var errorMessage = error.getDefaultMessage();
+            // 6:9:20
+            // errors.put(fieldName, errorMessage);
+            errors.put(fieldName,errorMessage);
+        });
+        return new ResponseEntity<>(errors,HttpStatus.BAD_REQUEST);
     }
 }
--- src/main/java/com/sizer/example/student/StudentDto.java
+++ src/main/java/com/sizer/example/student/StudentDto.java
@@ -1,7 +1,11 @@
 package com.sizer.example.student;
 
+import jakarta.validation.constraints.NotEmpty;
+
 public record StudentDto(
+        @NotEmpty(message = "Firstname should not be empty")
         String firstname,
+        @NotEmpty(message = "Lastname should not be empty")
         String lastname,
         String email,
         Integer schoolId) {
